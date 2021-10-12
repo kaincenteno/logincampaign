7526c73ff88d7757e2dc993134aaabbff7ca59fb
kain <kain.centeno@catsangel.com>
2021-10-12 15:30:24 -0700

    October's Campaign Table added
    Added October's campaign table. Thank you Metal from xiweb for
    collecting the information.

--- campaign_data.py
+++ campaign_data.py
@@ -98,6 +98,92 @@ data = {
                 }
             }
         },
+        10: {
+            1: {
+                10: {
+                    0: 1126,  # Beastmen's Seal
+                    1: 1127,  # Kindred's Seal
+                    2: 2955,  # Kindred's Crest
+                    3: 2956,  # High Kindred's Crest
+                    4: 2957,  # Sacred Kindred's Crest
+                    5: 1857,  # Cordial Invite
+                    6: 2306,  # Martial Ball Invite
+                    7: 5364,  # Training Grounds Key
+                    8: 2487,  # Mercenary Camp Entry Slip
+                    9: 5741,  # Flask Of Pest Repellent
+                    10: 3557,  # Athena Orb
+                    11: 5113,  # Cracked Nut
+                    12: 3541,  # Seasoning Stone
+                    13: 3543,  # Fossilized Fang
+                    14: 3542,  # Fossilized Bone
+                    15: 5724,  # Pungent Powder
+                    16: 6535,  # Pungent Powder II
+                },
+            },
+            5: {
+                100: {
+                    0: 8734,  # Mog Kupon I - S1
+                    1: 8966,  # Eudaemon Blade
+                    2: 8967,  # Eudaemon Cape
+                    3: 8968,  # Eudaemon Ring
+                    4: 8969,  # Eudaemon Sash
+                    5: 8970,  # Eudaemon Shield
+                    6: 17006,  # Drill Calamary
+                    7: 17007,  # Dwarf Pugil
+                    8: 26946,  # Pupil's Shirt
+                    9: 26964,  # Pupil's Camisa
+                    10: 27281,  # Pupil's Trousers
+                    11: 27455,  # Pupil's Shoes
+                },
+            },
+            9: {
+                300: {
+                    0: 3726,  # Aphmau Statue
+                    1: 10050,  # ♪Tiger companion
+                    2: 3651,  # Harvest Pastry
+                    3: 25713,  # Track Shirt
+                    4: 27325,  # Track Pants
+                    5: 25586,  # Kakai Cap
+                    6: 21820,  # Lost Sickle
+                    7: 3739,  # Autumn Tree
+                    8: 54,  # Chocobo Commode
+                },
+            },
+            13: {
+                500: {
+                    0: 3749,  # Chemistry Set
+                },
+            },
+            17: {
+                750: {
+                    0: 3885,  # Melodious Plans
+                    1: 3886,  # Timbre Case Kit
+                    2: 3887,  # Musichinery Kit
+                    3: 3339,  # Honey Wine
+                    4: 3341,  # Beastly Shank
+                    5: 3343,  # Blue Pondweed
+                    6: 6576,  # Turkey with Rolanberry Sauce
+                },
+            },
+            21: {
+                1000: {
+                    0: 6499,  # Patio Design Plans
+                    1: 26164,  # Caliber Ring
+                    2: 26165,  # Facility Ring
+                    3: 3640,  # Rol. Delightaru
+                    4: 3714,  # White Clematis
+                    5: 3715,  # Pink Clematis
+                    6: 3717,  # Birch Tree
+                },
+            },
+            25: {
+                1500: {
+                    0: 3340,  # Sweet Tea
+                    1: 3342,  # Savory Shank
+                    2: 3344,  # Red Pondweed
+                }
+            }
+        }
     },
 }
 
